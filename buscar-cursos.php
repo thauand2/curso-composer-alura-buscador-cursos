ae22d7a3d368102a744a869c1fb5c21e5b65df35
Thauan Silva <thauan.silva@diffucap.com.br>
2023-09-18 11:06:58 -0300

    Primeiro Commit

---
+++ buscar-cursos.php
@@ -0,0 +1,18 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Alura\BuscadorDeCursos\Buscador;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+$client = new Client(['verify' => false]);
+$crawler = new Crawler();
+
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador->buscar('https://alura.com.br/cursos-online-programacao/php');
+
+
+foreach($cursos as $curso){
+    echo exibeMensagem($curso);
+} 
